10900079ef1a3549029ba050fcc52a59e28e016d
JowayYoung <young.joway@aliyun.com>
2020-08-27 19:39:46 +0800

    增加项目基本骨架

--- README.md
+++
@@ -1,2 +0,0 @@
-# lottery
-基于React Hooks和Electron开发的跨平台幸运抽奖桌面应用
---
+++ main.js
@@ -0,0 +1,25 @@
+const Path = require("path");
+const { app, BrowserWindow } = require("electron");
+
+function CreateWindow() {
+	const mainWindow = new BrowserWindow({
+		height: 1200,
+		minHeight: 1200,
+		minWidth: 1600,
+		width: 1600
+	});
+	if (process.env.NODE_ENV === "dev") {
+		mainWindow.loadURL("http://127.0.0.1:9999");
+		mainWindow.webContents.openDevTools();
+	} else {
+		const entry = Path.join(__dirname, "./dist/test/index.html");
+		mainWindow.loadFile(entry);
+	}
+}
+
+// Electron初始完成并创建窗口时触发
+app.on("ready", CreateWindow);
+// Electron内部窗口闲置点击后重新创建窗口时触发
+app.on("activate", () => !BrowserWindow.getAllWindows().length && CreateWindow());
+// Electron内部所有窗口关闭时触发：MacOS中除非用户操作cmd+q显式退出，否则应用与菜单栏始终处于活动状态
+app.on("window-all-closed", () => process.platform !== "darwin" && app.quit());
